52b79c149530c668a1b617dcc15177144f6a0117
Tuyet Pham <phamtuyet201201@gmail.com>
2024-10-19 23:22:26 +0700

    first commit

---
+++ h26_mrp/__manifest__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+{
+    'name': 'h26_mrp',
+    'version': '15.0.1.0.0',
+    "sequence": 1,
+    'category': 'MRP',
+    'summary': 'Mrp',
+    'depends': [
+        ],
+    'data': [
+        ],
+    'installable': True,
+    'auto_install': False,
+    'application': False
+}
